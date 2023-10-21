34f9b83b274d9ee020e778e7ce3dd2fd51c0ba3e
koolo233 <koolo233@163.com>
2023-10-21 17:01:46 +0800

    Update Baseline

--- README.md
+++ README.md
@@ -1,16 +1,100 @@
 # PINNs for Solving Schrodinger Equation
 
-## Env
+## 运行环境
 
-## PINNs简介(杨子江)
-PINN基本原理
+首先需要使用conda配置环境，本Baseline基于Python 3.8，PyTorch1.13.0。初赛在个人电脑上，显存大于2G就能正常训练以及测试。
+下述命令如果出现类似于`http erro`或是`retry XXX`，请切换为国内源重试。
 
-## 利用PINNs求解PDE的基本流程（杨子江）
-PINN求解PDE的基本流程
+```commandline
+conda create -n pinn python=3.8
+conda activate pinn
+pip install torch==1.13.0+cu117 torchvision==0.14.0+cu117 torchaudio==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu117
+pip install -r requirements.txt
+```
 
-## Baseline详解（武晓蒙，杨子江）
-主要是介绍每一个函数的大致实现
+克隆本Baseline:
+```commandline
+git clone git@github.com:koolo233/Baseline4SolvingSchrodingerEquation.git
+cd Baseline4SolvingSchrodingerEquation
+```
+
+## 本Baseline简介
+
+本Baseline以基础PINN为模型，基于PyTorch实现了初赛的Baseline。包含从数据采样、模型构建、训练、测试以及生成提交文件的完整流程。大家可以在本Baseline的基础上进行修改，实现自己的想法。亦或是使用本Baseline的代码作为参考，实现自己的框架代码。
+
+对于PINN基础理论以及算法框架在此不多赘述，对这部分还有疑问的选手可以参考一并附带的参考文献以及PPT。
+
+本Baseline的代码注释比较详细，对于代码中的一些细节可以参考注释。
+
+各文件介绍如下：
+1. `main.py`：主文件，包含数据采样、模型构建、训练、测试以及生成提交文件的完整流程。
+2. `README.md`：本文件，包含本Baseline的简介以及使用方法。
+3. `requirements.txt`：包含本Baseline的依赖库。
+4. `test.csv`：测试数据集，与Kaggle上的一致
+5. `.gitignore`：git忽略文件
+6. `PINNs-组内专题.pptx`：PINNs的介绍PPT
+
+当运行本Baseline后会生成log文件夹，该文件夹中的文件介绍如下：
+1. `model.pth`：训练好的模型，可以直接用于测试。
+2. `baseline_submission.csv`：测试数据集的预测结果，可以直接提交到Kaggle。
+3. `loss_PINN.txt`：训练过程中的损失函数日志，可以用于绘制损失函数曲线。
+4. `loss_plot.png`：损失函数曲线，可以用于查看训练过程中的损失函数变化情况。
+5. `Collocation Points.png`：训练过程中的采样点分布，可以用于查看采样点的分布情况。
+
+这里需要额外介绍的是基础调参方法：
+1. 模型：模型的层数、神经元数量、激活函数、模型结构等
+2. 数据集：数据采样方法、数据集总数、mini-batch构造方法等
+3. 训练：学习率、训练轮数、优化器、学习率衰减等
+4. 损失函数：正则项、附加项等
+
+希望大家在本次初赛中掌握PINN求解PDE的基础方法的同时还能够对调参有一定的了解。
+
+最后：**Just Have Fun!**
+
+## 利用PINNs求解PDE的基本流程
+基本流程如下：
+
+| 流程                  | 代码位置            |
+|---------------------|-----------------|
+| 构建初始条件、边界条件以及PDE数据集 | main.py 64-135  |
+| 构建神经网络              | main.py 24-61   |
+| 定义基于PDE的损失计算组件      | main.py 178-242 |
+| 构建优化器等训练必要的组件       | main.py 165-171 |
+| 构建并执行训练循环           | main.py 138-278 |
+| 针对测试数据进行模型预测并保存结果   | main.py 281-301 |
+| main流程              | main.py 304-341 |
 
 ## 运行方法以及提交指南
 
-## 参考文献（杨子江、武晓蒙）
+```commandline
+# 切换到项目下
+cd Baseline4SolvingSchrodingerEquation
+# 训练并测试
+python main.py
+# 在服务器上如果需要指定显卡（示例为指定0卡，如果需要使用其他卡设置为其他数值），请使用
+CUDA_VISIBLE_DEVICES=0 python main.py
+# 提交问价将保存到log子文件下
+# 将baseline_submission.csv文件提交到Kaggle就完成了一次提交
+```
+
+## 参考文献
+
+-- Raissi, Maziar, Paris Perdikaris, and George E. Karniadakis. "Physics-informed neural networks: A deep learning framework for solving forward and inverse problems involving nonlinear partial differential equations." Journal of Computational physics 378 (2019): 686-707.
+
+-- Karniadakis, George Em, et al. "Physics-informed machine learning." Nature Reviews Physics 3.6 (2021): 422-440.
+
+-- Cai, Shengze, et al. "Physics-informed neural networks (PINNs) for fluid mechanics: A review." Acta Mechanica Sinica 37.12 (2021): 1727-1738.
+
+-- Yang, Zijiang, Zhongwei Qiu, and Dongmei Fu. "DMIS: dynamic mesh-based importance sampling for training physics-informed neural networks." Proceedings of the AAAI Conference on Artificial Intelligence. Vol. 37. No. 4. 2023.
+
+-- Lu, Lu, et al. "Physics-informed neural networks with hard constraints for inverse design." SIAM Journal on Scientific Computing 43.6 (2021): B1105-B1132.
+
+-- Yu, Jeremy, et al. "Gradient-enhanced physics-informed neural networks for forward and inverse PDE problems." Computer Methods in Applied Mechanics and Engineering 393 (2022): 114823.
+
+-- Yang, Liu, Xuhui Meng, and George Em Karniadakis. "B-PINNs: Bayesian physics-informed neural networks for forward and inverse PDE problems with noisy data." Journal of Computational Physics 425 (2021): 109913.
+
+-- Jin, Xiaowei, et al. "NSFnets (Navier-Stokes flow nets): Physics-informed neural networks for the incompressible Navier-Stokes equations." Journal of Computational Physics 426 (2021): 109951.
+
+-- Jagtap, Ameya D., Ehsan Kharazmi, and George Em Karniadakis. "Conservative physics-informed neural networks on discrete domains for conservation laws: Applications to forward and inverse problems." Computer Methods in Applied Mechanics and Engineering 365 (2020): 113028.
+
+-- Jagtap, Ameya D., Kenji Kawaguchi, and George Em Karniadakis. "Adaptive activation functions accelerate convergence in deep and physics-informed neural networks." Journal of Computational Physics 404 (2020): 109136.
--- main.py
+++ main.py
@@ -1,183 +1,343 @@
-#参考文献：
-#Physics-Informed Neural Networks_ A Deep Learning Framework for Solving Forward andInverse Problems Involving Nonlinear Partial Differential Equations
-#
+"""
+本文件为2023流光杯初赛Baseline。
+
+本文件包含完整的数据创建、采样、模型构建、训练以及输出预测文件等功能，用于选手学习实践PINNs。
+
+File Name: main.py
+Author: Xiaomeng Wu
+Created Date: 2023-10-
+"""
+
+import os
+import random
 import torch
 import torch.nn as nn
 import numpy as np
+import pandas as pd
 import torch.optim as optim
-import os
 import matplotlib.pyplot as plt
 
 # device
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 class PINN(nn.Module):
-    def __init__(self, num_layers, num_neurons, input_dim, output_dim):
+    """
+    PINN 模型定义
+    """
+    def __init__(self, num_layers, num_neurons, input_dim=2, output_dim=2):
+        """
+        模型初始化
+        :param num_layers: 总层数
+        :param num_neurons: 每一层神经元数量
+        :param input_dim: 输入维度
+        :param output_dim: 输出维度
+        """
         super(PINN, self).__init__()
+
+        # 输入层
         self.input_layer = nn.Linear(input_dim, num_neurons[0])
+
+        # 隐藏层
+        # 每一层由线性层和非线性激活函数构成
         layers = []
         for i in range(1, num_layers):
             layers.append(nn.Linear(num_neurons[i-1], num_neurons[i]))
             layers.append(nn.Tanh())
         self.hidden_layers = nn.Sequential(*layers)
+
+        # 输出层
         self.output_layer = nn.Linear(num_neurons[-1], output_dim)
 
     def forward(self, x):
+        """
+        正向forward函数
+        :param x: 输入数据
+        :return: 结果
+        """
         out = torch.tanh(self.input_layer(x))
         out = self.hidden_layers(out)
         out_final = self.output_layer(out)
         return out_final
 
-#初始值采样
-def create_data():
-    x_lower = -5
-    x_upper = 5
-    t_lower = 0
-    t_upper = np.pi/2
 
-    # 初始点采样数据
+def create_data():
+    """
+    创建用于监督PDE、Boundary以及Initial的数据集
+    :return: 创建的数据集
+    """
+
+    # 根据待求解方程确定x范围和t范围
+    x_lower = -5  # x最小值
+    x_upper = 5   # x最大值
+    t_lower = 0   # t最小值
+    t_upper = np.pi/2   # t最大值
+
+    # ----------
+    # Initial
+    # ----------
+    # 创建Initial Condition采样点数据集
+    # Initial Condition的t一定为0，x从[-5,5]随机采样
+    # 总样本点数为100
     t_initial = np.zeros((100, 1))
     x_initial = np.random.uniform(low=x_lower, high=x_upper, size=(100, 1))
-    t_initial_tensor = torch.from_numpy(t_initial).float().requires_grad_(True).to(device)
-    x_initial_tensor = torch.from_numpy(x_initial).float().requires_grad_(True).to(device)
-    initial_data = torch.cat([t_initial_tensor, x_initial_tensor],1)
+    # 生成tensor
+    t_initial_tensor = torch.from_numpy(t_initial).float().to(device)
+    x_initial_tensor = torch.from_numpy(x_initial).float().to(device)
+    initial_data = torch.cat([t_initial_tensor, x_initial_tensor], dim=1)
 
-    #边界点采样
-    x_boundary_lower = -5 * np.ones((100, 1))
-    x_boundary_upper = 5 * np.ones((100, 1))
-    t = np.random.uniform(low=t_lower, high=t_upper, size=(100, 1))
+    # ----------
+    # Boundary
+    # ----------
+    # 创建Boundary Condition采样点数据集
+    # 由于本次比赛的赛题为周期边界条件，因此需要在x的上下边界采样相同的x序列，t则在整个区间随机采样
+    # 总样本点数为100
+    x_boundary_lower = -5 * np.ones((100, 1))  # 下边界
+    x_boundary_upper = 5 * np.ones((100, 1))  # 上边界
+    t = np.random.uniform(low=t_lower, high=t_upper, size=(100, 1))  # 随机采样的t
+
+    # 创建tensor
     x_lower_tensor = torch.from_numpy(x_boundary_lower).float().requires_grad_(True).to(device)
     x_upper_tensor = torch.from_numpy(x_boundary_upper).float().requires_grad_(True).to(device)
     t_tensor = torch.from_numpy(t).float().requires_grad_(True).to(device)
     boundary_lower_data = torch.cat([t_tensor, x_lower_tensor],1)
     boundary_upper_data = torch.cat([t_tensor, x_upper_tensor],1)
 
-    # 残差点采样数据
+    # ----------
+    # PDE
+    # ----------
+    # 创建PDE采样点数据集
+    # 需要在区间内任意采样
+    # 总样本点数为2000
     x_collocation = np.random.uniform(low=x_lower, high=x_upper, size=(2000, 1))
     t_collocation = np.random.uniform(low=t_lower, high=t_upper, size=(2000, 1))
     x_collocation_tensor = torch.from_numpy(x_collocation).float().requires_grad_(True).to(device)
     t_collocation_tensor = torch.from_numpy(t_collocation).float().requires_grad_(True).to(device)
     pde_data = torch.cat([t_collocation_tensor, x_collocation_tensor],1)
+
+    # ----------
+    # 绘制采样结果
+    # ----------
+    plt.figure(figsize=(10, 8))
+    # 绘制Initial Condition采样点
+    plt.scatter(x_initial, t_initial, c='r', marker='o', label='Initial')
+    # 绘制Boundary Condition采样点
+    plt.scatter(x_boundary_lower, t, c='b', marker='o', label='Boundary')
+    plt.scatter(x_boundary_upper, t, c='b', marker='o')
+    # 绘制PDE采样点
+    plt.scatter(x_collocation, t_collocation, c='g', marker='o', label='PDE')
+    plt.xlabel('x')
+    plt.ylabel('t')
+    plt.title('Collocation Points')
+    plt.legend()
+    plt.savefig('./log/Collocation Points.png')
+    plt.clf()
+    plt.close()
+
     return pde_data, initial_data, boundary_lower_data, boundary_upper_data
 
 
-
 def train(model, pde_data, initial_data, boundary_lower_data, boundary_upper_data):
+    """
+    训练主函数
+    本函数中将定义损失函数、优化器以及训练过程
+    :param model: PINN模型
+    :param pde_data: PDE数据
+    :param initial_data: Initial数据
+    :param boundary_lower_data: 下边界数据
+    :param boundary_upper_data: 上边界数据
+    :return: None
+    """
 
-    #定义导函数
-    def gradients(output, input_tensor):
-        gradients = torch.autograd.grad(outputs=output, inputs=input_tensor, grad_outputs=torch.ones_like(output),
-                                        create_graph=True)[0]
-        return gradients
+    def gradients(_output, _input_tensor):
+        """
+        梯度计算
+        :param _output: 输出tensor
+        :param _input_tensor: 输入tensor
+        :return: 输出对输入的梯度计算结果
+        """
+        _gradients = torch.autograd.grad(
+            outputs=_output,
+            inputs=_input_tensor,
+            grad_outputs=torch.ones_like(_output),
+            create_graph=True
+        )[0]
+        return _gradients
 
+    losses = []  # 损失记录器
+    initial_losses = []  # 初始条件损失记录器
+    boundary_losses = []  # 边界条件损失记录器
+    pde_losses = []  # PDE损失记录器
+
+    optimizer = optim.Adam(model.parameters(), lr=0.001)  # 初始化优化器
+    MAX_STEPS = 20  # 最大训练步数
     model.train()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    epochs = 20
-    losses = []
-    initial_losses = []
-    boundary_losses = []
-    pde_losses = []
-    for epoch in range(1, epochs + 1):
+
+    # 训练主循环
+    for step in range(1, MAX_STEPS + 1):
         optimizer.zero_grad()
 
-        # initial loss
-        output_initial = model(initial_data)
-        initial_value = 2 / torch.cosh(initial_data[:, 1:2])
-        initial_loss_real = torch.mean((output_initial[:, 0:1] - initial_value) ** 2)
-        initial_loss_imag = torch.mean((output_initial[:, 1:2]) ** 2)
-        initial_loss = initial_loss_real + initial_loss_imag
+        # -------------
+        # 初始条件损失
+        # -------------
+        output_initial = model(initial_data)  # 预测
+        initial_value = 2 / torch.cosh(initial_data[:, 1:2])  # 初始条件真实值
+        initial_loss_real = torch.mean((output_initial[:, 0:1] - initial_value) ** 2)  # 实部
+        initial_loss_imag = torch.mean((output_initial[:, 1:2]) ** 2)  # 虚部
+        initial_loss = initial_loss_real + initial_loss_imag  # 初始条件损失
 
-        #boundary loss
-        output_lower = model(boundary_lower_data)
-        output_upper = model(boundary_upper_data)
+        # -------------
+        # 边界条件损失
+        # -------------
+        output_lower = model(boundary_lower_data)  # 下边界预测结果
+        output_upper = model(boundary_upper_data)  # 上边界预测结果
 
+        # 下边界实部梯度
         df_dx_lower_real = gradients(output_lower[:, 0:1], boundary_lower_data)[:, 1:2]
+        # 下边界虚部梯度
         df_dx_lower_imag = gradients(output_lower[:, 1:2], boundary_lower_data)[:, 1:2]
 
+        # 上边界实部梯度
         df_dx_upper_real = gradients(output_upper[:, 0:1], boundary_upper_data)[:, 1:2]
+        # 上边界虚部梯度
         df_dx_upper_imag = gradients(output_upper[:, 1:2], boundary_upper_data)[:, 1:2]
 
+        # 周期边界条件：直接数值损失
         boundary_value_loss_real = torch.mean((output_lower[:, 0:1] - output_lower[:, 0:1]) ** 2)
         boundary_value_loss_imag = torch.mean((output_lower[:, 1:2] - output_lower[:, 1:2]) ** 2)
 
+        # 周期边界条件：梯度损失
         boundary_gradient_loss_real = torch.mean((df_dx_lower_real - df_dx_upper_real) ** 2)
         boundary_gradient_loss_imag = torch.mean((df_dx_lower_imag - df_dx_upper_imag) ** 2)
 
-        boundary_loss = boundary_value_loss_real + boundary_value_loss_imag + \
-                              boundary_gradient_loss_real + boundary_gradient_loss_imag
+        # 总损失
+        boundary_loss = boundary_value_loss_real + boundary_value_loss_imag +\
+                        boundary_gradient_loss_real + boundary_gradient_loss_imag
 
-        #PDE loss
-        output = model(pde_data)
-        output_real = output[:, 0:1]
-        output_imag = output[:, 1:2]
+        # -------------
+        # PDE损失
+        # -------------
+        output = model(pde_data)  # PDE监督样本结果
+        output_real = output[:, 0:1]  # 实部预测结果
+        output_imag = output[:, 1:2]  # 虚部预测结果
 
+        # 计算实部对输入的一阶梯度
         df_dtx_real = gradients(output_real, pde_data)
         df_dt_real = df_dtx_real[:, 0:1]
         df_dx_real = df_dtx_real[:, 1:2]
 
+        # 计算虚部对输入一阶梯度
         df_dtx_imag = gradients(output_imag, pde_data)
         df_dt_imag = df_dtx_imag[:, 0:1]
         df_dx_imag = df_dtx_imag[:, 1:2]
 
+        # 计算实部对输入坐标x的二阶梯度
         df_dxx_real = gradients(df_dx_real, pde_data)[:, 1:2]
+        # 计算虚部对输入坐标x的二阶梯度
         df_dxx_imag = gradients(df_dx_imag, pde_data)[:, 1:2]
 
+        # 计算实部PDE损失
         pde_real = -df_dt_imag + 0.5 * df_dxx_real + (output_real ** 2 + output_imag ** 2) * output_real
+        # 计算虚部PDE损失
         pde_imag = df_dt_real + 0.5 * df_dxx_imag + (output_real ** 2 + output_imag ** 2) * output_imag
+        # 计算总损失
         pde_loss = torch.mean(pde_real**2)+torch.mean(pde_imag**2)
 
-        # 将误差(损失)累加起来
+        # 误差累加
         loss = initial_loss + boundary_loss + pde_loss
 
-        loss.backward()
-        optimizer.step()
+        loss.backward()  # 反向传播
+        optimizer.step()  # 参数优化
+
+        # 记录损失值
         losses.append(loss.item())
         initial_losses.append(initial_loss.item())
         boundary_losses.append(boundary_loss.item())
         pde_losses.append(pde_loss.item())
 
-        if epoch % 10 == 0:
-            if not os.path.exists('log'):
-                os.makedirs('log')
-            with open('log/loss_PiNN.txt', 'a') as f:
-                f.write(
-                    'Epoch [{}/{}], Loss: {:.4f}, initial_loss: {:.8f}, boundary_loss: {:.8f}, pde_loss:{:.4f}\n'.format(
-                        epoch + 1, epochs, loss.item(), initial_loss.item(), boundary_loss.item(),pde_loss.item()))
+        # 输出字符串
+        print_str = 'Step [{}/{}], Loss: {:.4e}, initial_loss: {:.4e}, ' \
+                    'boundary_loss: {:.4e}, pde_loss:{:.4e}'.format(
+            MAX_STEPS, step, loss.item(), initial_loss.item(), boundary_loss.item(), pde_loss.item()
+        )
+        # 输出到log文件
+        with open('log/loss_PINN.txt', 'a') as f:
+            f.write(print_str + '\n')
+        # 输出到cmd
+        print(print_str)
+
+    # 保存模型
+    torch.save(model.state_dict(), "./log/model.pth")
 
     # 绘制损失图像
     plt.plot(range(1, len(losses) + 1), losses, label='Loss')
     plt.plot(range(1, len(pde_losses) + 1), pde_losses, label='pde_loss')
     plt.plot(range(1, len(boundary_losses) + 1), boundary_losses, label='boundary_loss')
     plt.plot(range(1, len(initial_losses) + 1), initial_losses, label='initial_loss')
-    plt.xlabel('Epoch')
+    plt.xlabel('Step')
     plt.ylabel('Loss')
     plt.legend()
     plt.savefig('log/loss_plot.png')
-def pred_and_valuation(test):
-    test_input_t = torch.from_numpy(test['input_t']).float().to(device)
-    test_input_x = torch.from_numpy(test['input_x']).float().to(device)
-    test_input_tensor = torch.stack([test_input_t, test_input_x], dim=1)
-    test_output = test['output']
-    with torch.no_grad():
-        model.eval()
-        pred_output = model(test_input_tensor.to(device)).detach().cpu().numpy()
-        pred_length = np.sqrt(np.mean(pred_output[:, 0]**2)+np.mean(pred_output[:, 1]**2))
-    L1 = np.linalg.norm(test_output - pred_length, 1)
-    L2 = np.linalg.norm(test_output - pred_length)
-    max_error = np.max(np.abs(pred_length-test_output))
-    min_error = np.min(np.abs(pred_length-test_output))
-    average_error = np.average(np.abs(pred_length-test_output))
 
-    with open('log/pre_output.txt', 'a') as f:
-        f.write(
-            'l1: {:.8f}, l2: {:.8f}, max_error: {:.8f}, min_error:{:.8f}, average_error:{:.8f}\n'.format(
-                L1, L2, max_error, min_error.item(), average_error))
+
+def testing(test):
+
+    # 创建test输入数据
+    test_input_t = torch.from_numpy(np.array(test['t'])).float().to(device)
+    test_input_x = torch.from_numpy(np.array(test['x'])).float().to(device)
+    test_input_tensor = torch.stack([test_input_t, test_input_x], dim=1)
+
+    # 模型推理
+    model.eval()
+    with torch.no_grad():
+        pred = model(test_input_tensor.to(device)).detach()
+        pred = torch.sqrt(pred[:, 0:1] ** 2 + pred[:, 1:2] ** 2).cpu().numpy()
+    pred = pred.reshape(-1)
+
+    # 输出到csv
+    df = pd.DataFrame()  # 创建DataFrame
+    df["id"] = range(test['t'].shape[0])  # 创建id列
+    df["t"] = test['t']  # 创建t列
+    df["x"] = test['x']  # 创建x列
+    df["pred"] = pred  # 创建pred列
+    df.to_csv("log/baseline_submission.csv", index=False)
+
 
 if __name__ == '__main__':
-    torch.manual_seed(0)
-    np.random.seed(0)
+    # ----------
+    # 固定随机数
+    # ----------
+    np.random.seed(0)  # numpy seed
+    random.seed(0)  # random seed
+    torch.manual_seed(0)  # pytorch seed
+    torch.cuda.manual_seed(0)  # pytorch gpu seed
+
+    # 创建日志文件夹
+    if not os.path.exists('log'):
+        os.makedirs('log')
+
+    # ----------
+    # 生成训练数据
+    # ----------
     pde_data, initial_data, boundary_lower_data, boundary_upper_data = create_data()
-    model = PINN(num_layers=4, num_neurons=[100, 100, 100, 100], input_dim=2, output_dim=2).to(device)
+    print("Init data done...")
+
+    # ----------
+    # 创建模型
+    # ----------
+    model = PINN(num_layers=4, num_neurons=[100, 100, 100, 100]).to(device)
+    print("Init model done...")
+    print(model)
+
+    # ----------
+    # 训练主循环
+    # ----------
     train(model, pde_data, initial_data, boundary_lower_data, boundary_upper_data)
-    test = np.load('schrodingerEquation_GT.npz')
-    pred_and_valuation(test)
+    print("Training done...")
+
+    # ----------
+    # 测试主循环
+    # ----------
+    testing(pd.read_csv('test.csv'))  # prediction
+    print("Testing Done...")
+    print("prediction file is saved as log/baseline_submission.csv")
